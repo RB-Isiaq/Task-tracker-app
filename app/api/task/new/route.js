ee964582896b31bb75d283a66a03a523d9753f74
Isiaq Ridwan Bukola <99425435+RB-Isiaq@users.noreply.github.com>
2023-06-23 23:40:02 +0100

    Update route.js

--- app/api/task/new/route.js
+++ app/api/task/new/route.js
@@ -1,1 +1,23 @@
+import { connectToDB } from "@utils/database";
+import Task from "@models/task";
+import { NextResponse } from "next/server";
 
+export const POST = async (req) => {
+  const body = await req.json();
+
+  try {
+    await connectToDB();
+    const newTask = new Task(body);
+
+    await newTask.save();
+
+    return new NextResponse("Task has been created", {
+      status: 201,
+    });
+    
+  } catch (error) {
+    return new NextResponse("Failed to create a new Task", {
+      status: 500,
+    });
+  }
+};
