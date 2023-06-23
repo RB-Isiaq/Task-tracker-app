949ee820d4885994cb359843820307821502e407
Isiaq Ridwan Bukola <99425435+RB-Isiaq@users.noreply.github.com>
2023-06-23 19:07:58 +0100

    Update updateTaskpage.jsx

--- app/dashboard/[taskDetail]/update-prompt/page.jsx
+++ app/dashboard/[taskDetail]/update-prompt/page.jsx
@@ -5,50 +5,56 @@ import { data } from "@components";
 
 import Form from "@components/Form";
 
-const EditPrompt = () => {
+const EditTask = () => {
   const router = useRouter();
   const pathName = usePathname();
   console.log(pathName);
-  const dataID = pathName.split("/").splice(2, 1).join("");
-  console.log(dataID);
-  const dataEdit = data.filter((item) => item.id === +dataID);
+  const taskID = pathName.split("/").splice(2, 1).join("");
+  console.log(taskID);
+  const taskEdit = data.filter((item) => item.id === +taskID);
   console.log(dataEdit);
   const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
+  const [task, setTask] = useState({
+    title: "",
+    desc: "",
+    dueDate: "",
+    status: ""
   });
 
   useEffect(() => {
-    const getPromptDetails = async () => {
+    const getTaskDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
 
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
+      setTask({
+        title: data.title,
+        desc: data.desc,
+        dueDate: data.dueDate,
+        status: data.status
       });
     };
 
-    if (dataID) getPromptDetails();
+    if (taskID) getTaskDetails();
   }, []);
 
-  const updatePrompt = async (e) => {
+  const updateTask = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
-    if (!promptId) alert("Prompt ID not found");
+    if (!taskID) alert("Task ID not found");
     try {
       const response = await fetch(`/api/prompt/${promptID}`, {
         method: "PATCH",
         body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
+          title: data.title,
+        desc: data.desc,
+        dueDate: data.dueDate,
+        status: data.status
         }),
       });
 
       if (response.ok) {
-        router.push("/");
+        router.push("/dashboard");
       }
     } catch (error) {
       console.log(error);
@@ -59,12 +65,12 @@ const EditPrompt = () => {
   return (
     <Form
       type="Edit"
-      data={dataEdit[0]}
-      setPost={setPost}
+      task={taskEdit[0]}
+      setTask={setTask}
       submitting={submitting}
-      handleSubmit={updatePrompt}
+      handleSubmit={updateTask}
     />
   );
 };
 
-export default EditPrompt;
+export default EditTask;
