a9e4c1a4ce2445906fa8338ec9f07ec45021446c
Isiaq Ridwan Bukola <99425435+RB-Isiaq@users.noreply.github.com>
2023-06-24 15:27:11 +0100

    Update route.js

--- app/api/task/route.js
+++ app/api/task/route.js
@@ -1,11 +1,12 @@
 import Task from "@models/task";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request) => {
+export const GET = async (request, {params}) => {
+    const id = params.id.toString();
     try {
         await connectToDB()
 
-        const tasks = await Task.find({}).populate('userId')
+        const tasks = await Task.find({userId: id})
 
         return new Response(JSON.stringify(tasks), { status: 200 })
     } catch (error) {
