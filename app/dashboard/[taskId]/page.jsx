200726df5ec8c605d65a2d416e4c2d20c5a7fb31
Isiaq Ridwan Bukola <99425435+RB-Isiaq@users.noreply.github.com>
2023-06-25 11:18:01 +0100

    Update page.jsx

--- app/dashboard/[taskId]/page.jsx
+++ app/dashboard/[taskId]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import TaskDetails from "@components/TaskDetails";
 import { useParams, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useSWR from "swr";
 
 const TaskDetail = () => {
