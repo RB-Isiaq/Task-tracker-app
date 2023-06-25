519828198251cf42614c9304634ac37976c5219a
Isiaq Ridwan Bukola <99425435+RB-Isiaq@users.noreply.github.com>
2023-06-25 13:30:38 +0100

    Create page.jsx

---
+++ app/dashboard/update-task/[taskId]/page.jsx
@@ -0,0 +1,84 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter, useParams } from "next/navigation";
+
+import Form from "@components/Form";
+
+const UpdateTask = () => {
+  const router = useRouter();
+  const {data: session} = useSession();
+  const {taskId} = useParams();
+  const [submitting, setSubmitting] = useState(false);
+  const [task, setTask] = useState((});
+
+  console.log(taskId)
+  
+  const getTaskDetails = async () => {
+      const response = await fetch(`/api/task/${taskId}`);
+      const data = await response.json();
+
+      setTask(data);
+    };
+  useEffect(() => {
+
+    // if (taskId) getTaskDetails();
+    getTaskDetails();
+  }, []);
+
+  const updateTask = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    const form = new Form(e.target);
+    const formData = Object.fromEntries(form.entries());
+    formData.userId = session?.user.id;
+    console.log(formData);
+    // if (!taskId) alert("Task ID not found");
+    try {
+      const res = await fetch(`/api/task/${taskId}`, {
+        method: "PATCH",
+        body: JSON.stringify(formData),
+      });
+      console.log(res);
+      res.ok === true && router.push("/dashboard");
+    } catch (error) {
+      setError(error);
+      console.log(error);
+      alert(error);
+    } finally {
+      setSubmitting(false);
+    }
+
+    // if (!taskId) alert("Task ID not found");
+    // try {
+    //   const response = await fetch(`/api/prompt/${promptID}`, {
+    //     method: "PATCH",
+    //     body: JSON.stringify({
+    //       title: data.title,
+    //       desc: data.desc,
+    //       dueDate: data.dueDate,
+    //       status: data.status,
+    //     }),
+    //   });
+
+    //   if (response.ok) {
+    //     router.push("/dashboard");
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // } finally {
+    //   setSubmitting(false);
+    // }
+  };
+  return (
+    <Form
+      type="Edit"
+      task={task}
+      setTask={setTask}
+      submitting={submitting}
+      handleSubmit={updateTask}
+    />
+  );
+};
+
+export default UpdateTask;
