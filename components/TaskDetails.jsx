c91b7c8c9e302b2a45a67b91787108678e79847c
Isiaq Ridwan Bukola <99425435+RB-Isiaq@users.noreply.github.com>
2023-06-25 08:38:01 +0100

    Update TaskDetails.jsx

--- components/TaskDetails.jsx
+++ components/TaskDetails.jsx
@@ -4,11 +4,11 @@ const TaskDetails = ({ data, deleteHandler }) => {
   return (
     <section className="flex flex-col justify-center items-center h-screen">
       <div className="flex flex-col gap-6 justify-center items-center py-10 px-4 glassmorphism">
-        <h1 className="capitalize text-center sm:font-semibold text-lg sm:text-2xl mb-4">
+        <h1 className="capitalize text-center font-semibold text-2xl mb-4">
           {data?.title}
         </h1>
         <div className="w-full">{data?.desc}</div>
-        <div className="flex justify-between items-center w-full">
+        <div className="flex justify-between gap-8 items-center w-full">
           <div className="flex gap-2 items-center">
             <div
               className={`w-[10px] h-[10px] rounded-full ${
